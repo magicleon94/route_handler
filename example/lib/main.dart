4489ea9de5e541440c2c1e28e7f82552ccdd4349
Antonello Galipò <magicleon94@gmail.com>
2020-08-18 18:52:43 +0200

    Initial release

---
+++ example/lib/main.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:route_handler/route_handler.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: (settings) => Routes.onGenerateRoute(settings, context),
+    );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              child: Text("Query param example"),
+              onPressed: () => Navigator.of(context)
+                  .pushNamed(Routes.queryParam + '?id=1&name=foo&surname=bar'),
+            ),
+            RaisedButton(
+              child: Text("Route param example"),
+              onPressed: () => Navigator.of(context).pushNamed(
+                Routes.routeParam,
+                arguments: RouteParameter('1', 1),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class QueryParamPage extends StatelessWidget {
+  final Map<String, String> queryParams;
+
+  const QueryParamPage({Key key, this.queryParams}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Query params page'),
+      ),
+      body: Center(
+        child: Text(
+          queryParams.toString(),
+        ),
+      ),
+    );
+  }
+}
+
+class RouteParameterPage extends StatelessWidget {
+  final RouteParameter parameter;
+
+  const RouteParameterPage({Key key, this.parameter}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Route parameter page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('String value: ${parameter.stringValue}'),
+            Text('Int value: ${parameter.intValue}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Routes {
+  static const root = '/';
+  static const queryParam = '$root/queryParam';
+  static const routeParam = '$root/routeParam';
+
+  static Route onGenerateRoute(RouteSettings settings, [BuildContext context]) {
+    var route = settings.name.split('?').first;
+
+    switch (route) {
+      case queryParam:
+        return QueryParamHandler(settings).getRoute(context);
+      case routeParam:
+        final parameter = settings.arguments as RouteParameter;
+        return RouteParamHandler(settings, parameter).getRoute(context);
+      case root:
+      default:
+        return RootHandler(settings).getRoute(context);
+    }
+  }
+}
+
+class RootHandler extends RouteHandler {
+  RootHandler(RouteSettings settings) : super(settings);
+
+  @override
+  Route getRoute(BuildContext context) {
+    return MaterialPageRoute(builder: (_) => RootPage());
+  }
+}
+
+class QueryParamHandler extends RouteHandler {
+  QueryParamHandler(RouteSettings settings) : super(settings);
+
+  @override
+  Route getRoute(BuildContext context) {
+    return MaterialPageRoute(
+      builder: (_) => QueryParamPage(
+        queryParams: queryParameters,
+      ),
+    );
+  }
+}
+
+class RouteParamHandler extends RouteHandler<RouteParameter> {
+  RouteParamHandler(RouteSettings settings, RouteParameter extra)
+      : super(
+          settings,
+          routeExtra: extra,
+        );
+
+  @override
+  Route getRoute(BuildContext context) {
+    return MaterialPageRoute(
+      builder: (_) => RouteParameterPage(
+        parameter: routeExtra,
+      ),
+    );
+  }
+}
+
+class RouteParameter {
+  final String stringValue;
+  final int intValue;
+
+  RouteParameter(this.stringValue, this.intValue);
+}
---
+++ lib/route_handler.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/widgets.dart';
+
+/// [RouteHandler abstract class]
+abstract class RouteHandler<T> {
+  ///[settings] are the `RouteSettings` passed by the `onGenerateRoute` from `MaterialApp`
+  final RouteSettings settings;
+
+  ///[routeExtra] is an optional extra if a complex object needs to be passed to the route
+  final T routeExtra;
+
+  @mustCallSuper
+  RouteHandler(this.settings, {this.routeExtra});
+
+  /// Returns the generated route
+  Route getRoute(BuildContext context);
+
+  /// Parse the query parameters passed in the route name
+  /// Example: routeName?id=1&name=foo will result in
+  /// {'id': '1', 'name':'foo'}
+  Map<String, String> get queryParameters {
+    final urlParts = settings.name.split('?');
+    if (urlParts.length > 1) {
+      final paramPart = urlParts[1];
+      final params = <String, String>{};
+      paramPart.split('&').forEach((p) {
+        final split = p.split('=');
+        final key = split[0];
+        final value = split[1];
+        params[key] = value;
+      });
+      return params;
+    }
+
+    return {};
+  }
+}
